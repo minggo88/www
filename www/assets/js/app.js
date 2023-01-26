f24e5d19aa6ae5d77b59b7bc86e03d1c07e04e40
minggo88 <117075379+minggo88@users.noreply.github.com>
2023-01-26 17:01:50 +0900

    통장인증 이벤트 추가

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1698,6 +1698,12 @@ translate();// head 에서 번역처리 할때 누락된것들이 있어 HMLT �
 			$('.flex-self-left p').hide();
 			$('.text-black').hide();
 			$('[name=btn_save]').hide();
+			
+			$('.#account_ing').hide();
+			$('.#account_ing2').hide();
+			$('.#account_ing3').hide();
+			$('.#account_ing4').hide();
+			$('.#account_success').show();
 		} else {
 			if (Model.user_info.image_bank_url) { // 신분증 인증 대기중
 				$('[name=status_waiting]').show();
@@ -2410,6 +2416,12 @@ translate();// head 에서 번역처리 할때 누락된것들이 있어 HMLT �
 			$('.flex-self-left p').hide();
 			$('.text-black').hide();
 			$('[name=btn_save]').hide();
+			
+			$('.#account_ing').hide();
+			$('.#account_ing2').hide();
+			$('.#account_ing3').hide();
+			$('.#account_ing4').hide();
+			$('.#account_success').show();
 		} else {
 			if (Model.user_info.image_identify_url) { // 신분증 인증 대기중
 				$('[name=status_waiting]').show();
