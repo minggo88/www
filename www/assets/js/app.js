87c3838f6da4c85b0d27d7949c2841ca97936e2d
minggo88 <117075379+minggo88@users.noreply.github.com>
2023-01-28 00:04:00 +0900

    수익률 추가

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -2645,6 +2645,8 @@ translate();// head 에서 번역처리 할때 누락된것들이 있어 HMLT �
 						const avg_price_one = real_number_format(item.avg_buy_price,1);
 						const avg_price = item.avg_buy_price*item.confirmed_str;
 						const avg_price_num = real_number_format(avg_price,1);
+						const income_rate = (item.currency_price-avg_price) / avg_price * 100;
+						income_rate = income_rate.toFixed(2);
 						
 						const grid = $(`<div class="grid" style="border-left-color: #${item.color};" />`)
 						grid.append(`
@@ -2683,6 +2685,7 @@ translate();// head 에서 번역처리 할때 누락된것들이 있어 HMLT �
 								</div>
 								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
 									<div class=my_income style ="color: var(--red-up) !important;padding-right: 25px;"> ${item_income}</div>
+									<div class=my_income_rate style ="color: var(--red-up)  !important;padding-right: 25px;"> ${income_rate}</div>
 								</div>
 							</div>
 						`)
@@ -2700,6 +2703,7 @@ translate();// head 에서 번역처리 할때 누락된것들이 있어 HMLT �
 								</div>
 								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
 									<div class=my_income style ="color: var(--blue-dn) !important;padding-right: 25px;"> ${item_income}</div>
+									<div class=my_income_rate style ="color: var(--blue-dn) !important;padding-right: 25px;"> ${income_rate}</div>
 								</div>
 							</div>
 						`)
@@ -2717,6 +2721,7 @@ translate();// head 에서 번역처리 할때 누락된것들이 있어 HMLT �
 								</div>
 								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
 									<div class=my_income style="padding-right: 25px;"> ${item_income}</div>
+									<div class=my_income style="padding-right: 25px;"> ${income_rate}</div>
 								</div>
 							</div>
 							
