e87cf619cd6b5ffda67274d8b0a8ac23ff281ea6
minggo88 <117075379+minggo88@users.noreply.github.com>
2023-01-27 23:42:36 +0900

    내지갑 위치 수정

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -2640,6 +2640,7 @@ translate();// head 에서 번역처리 할때 누락된것들이 있어 HMLT �
 						const item_name = item.name;
 						const item_price = real_number_format(item.currency_price,1);
 						const item_income = real_number_format(item.eval_income,1);
+						const item_total = item_price * ${item.confirmed_str};
 						const item_grade = item.goods_grade;
 						const avg_price_one = real_number_format(item.avg_buy_price,1);
 						const avg_price = item.avg_buy_price*item.confirmed_str;
@@ -2672,15 +2673,16 @@ translate();// head 에서 번역처리 할때 누락된것들이 있어 HMLT �
 						grid.append(`
 							
 								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
-									<div class="wallet--price">${item_price}</div>
-									${item.symbol !== exchange ? '<div class="wallet--market-price">≈ '+real_number_format(item.eval_valuation_str,1)+'</div>' : ''}
-								</div>
-								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
+									<div class="wallet--price">${item_price} </div>
 									<div class="item--avg--price">${avg_price_one}</div>
-									${item.symbol !== exchange ? '<div class="wallet--market-price">≈ '+real_number_format(avg_price_num,1) +'</div>' : ''}
 								</div>
 								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
-									<div class=my_income style ="color: var(--red-up) !important;"> ${item_income}</div>
+									
+									${item.symbol !== exchange ? '<div class="wallet--rice"> '+real_number_format(item_total,1)+'</div>' : ''}
+									${item.symbol !== exchange ? '<div class="wallet--price"> '+real_number_format(avg_price_num,1) +'</div>' : ''}
+								</div>
+								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
+									<div class=my_income style ="color: var(--red-up) !important;padding-right: 25px;"> ${item_income}</div>
 								</div>
 							</div>
 						`)
@@ -2688,15 +2690,16 @@ translate();// head 에서 번역처리 할때 누락된것들이 있어 HMLT �
 						grid.append(`
 							
 								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
-									<div class="wallet--price">${item_price}</div>
-									${item.symbol !== exchange ? '<div class="wallet--market-price">≈ '+real_number_format(item.eval_valuation_str,1)+'</div>' : ''}
-								</div>
-								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
+									<div class="wallet--price">${item_price} </div>
 									<div class="item--avg--price">${avg_price_one}</div>
-									${item.symbol !== exchange ? '<div class="wallet--market-price">≈ '+real_number_format(avg_price_num,1) +'</div>' : ''}
 								</div>
 								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
-									<div class=my_income style ="color: var(--blue-dn) !important;"> ${item_income}</div>
+									
+									${item.symbol !== exchange ? '<div class="wallet--rice"> '+real_number_format(item_total,1)+'</div>' : ''}
+									${item.symbol !== exchange ? '<div class="wallet--price"> '+real_number_format(avg_price_num,1) +'</div>' : ''}
+								</div>
+								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
+									<div class=my_income style ="color: var(--blue-dn) !important;padding-right: 25px;"> ${item_income}</div>
 								</div>
 							</div>
 						`)
@@ -2704,15 +2707,16 @@ translate();// head 에서 번역처리 할때 누락된것들이 있어 HMLT �
 						grid.append(`
 							
 								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
-									<div class="wallet--price">${item_price}</div>
-									${item.symbol !== exchange ? '<div class="wallet--market-price">≈ '+real_number_format(item.eval_valuation_str,1)+'</div>' : ''}
-								</div>
-								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
+									<div class="wallet--price">${item_price} </div>
 									<div class="item--avg--price">${avg_price_one}</div>
-									${item.symbol !== exchange ? '<div class="wallet--market-price">≈ '+real_number_format(avg_price_num,1) +'</div>' : ''}
 								</div>
 								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
-									<div class=my_income> ${item_income}</div>
+									
+									${item.symbol !== exchange ? '<div class="wallet--rice"> '+real_number_format(item_total,1)+'</div>' : ''}
+									${item.symbol !== exchange ? '<div class="wallet--price"> '+real_number_format(avg_price_num,1) +'</div>' : ''}
+								</div>
+								<div class="text-right" style="display: flex; flex-basis: 100%; flex-direction: column; column-gap: 5px; justify-content: flex-start">
+									<div class=my_income style="padding-right: 25px;"> ${item_income}</div>
 								</div>
 							</div>
 							
