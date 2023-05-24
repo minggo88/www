998712dff9d802846a9d418d8293714b9dc8ec21
kmg <flyminggo@naver.com>
2023-05-24 16:49:31 +0900

    오류 수정

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1769,7 +1769,7 @@ translate();// head 에서 번역처리 할때 누락된것들이 있어 HMLT �
 			$('[name="btn_save"]').show();*/
 
 			console.log('1111');
-			console.log(unserialize($('#change-account-number').serialize());
+			console.log(unserialize($('#change-account-number').serialize()));
 			add_request_item('checkAccount', unserialize($('#change-account-number').serialize()), function(r) {
 				if (r?.success) {
 					for (var key in r) {
