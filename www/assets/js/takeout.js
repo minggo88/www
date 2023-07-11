2934fa776a61598d6080bc0389d6cae61fb590cc
kmg <flyminggo@naver.com>
2023-07-11 18:30:55 +0900

    take-out update2

--- www/assets/js/takeout.js
+++ www/assets/js/takeout.js
@@ -223,41 +223,43 @@ $(document).ready(function() {
             })
         }
     });
+
+    let selectFlag;
+    $('.custom-select').on('click', function() {
+      $(this).toggleClass('selected');
+      if($(this).hasClass('selected')) {
+        $('.custom-select-list').show();
+      } else {
+        $('.custom-select-list').hide();
+      }
+    })
+    
+    $('.custom-select').on('focusin', function() {
+      $('.custom-select-list').show();
+    });
+    
+    $('.custom-select').on('focusout', function() {
+      if(!selectFlag) {
+        $('.custom-select-list').hide();
+      }
+      $(this).removeClass('selected');
+    });
+    
+    $('.custom-select-option').on('mouseenter', function() {
+      selectFlag = true;
+    });
+    
+    $('.custom-select-option').on('mouseout', function() {
+      selectFlag = false;
+    });
+    
+    $('.custom-select-option').on('click', function() {
+      let value = $(this).attr('value');
+    
+      $('.custom-select-text').text($(this).text());
+      $('.select-origin').val(value);
+      $('.custom-select-list').hide();
+    });
+
 });
 
-let selectFlag;
-$('.custom-select').on('click', function() {
-  $(this).toggleClass('selected');
-  if($(this).hasClass('selected')) {
-    $('.custom-select-list').show();
-  } else {
-    $('.custom-select-list').hide();
-  }
-})
-
-$('.custom-select').on('focusin', function() {
-  $('.custom-select-list').show();
-});
-
-$('.custom-select').on('focusout', function() {
-  if(!selectFlag) {
-    $('.custom-select-list').hide();
-  }
-  $(this).removeClass('selected');
-});
-
-$('.custom-select-option').on('mouseenter', function() {
-  selectFlag = true;
-});
-
-$('.custom-select-option').on('mouseout', function() {
-  selectFlag = false;
-});
-
-$('.custom-select-option').on('click', function() {
-  let value = $(this).attr('value');
-
-  $('.custom-select-text').text($(this).text());
-  $('.select-origin').val(value);
-  $('.custom-select-list').hide();
-});
