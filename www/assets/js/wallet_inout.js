c008024e9f38a89187caed6de3c6a9da834919e5
minggo88 <117075379+minggo88@users.noreply.github.com>
2023-11-08 15:55:36 +0900

    wallet inout update

--- www/assets/js/wallet_inout.js
+++ www/assets/js/wallet_inout.js
@@ -5,31 +5,87 @@ setTimeout(function() {
 
 var search_type = 0;
 
+const fn_total_money = function(){
+	API.getBalance('ALL', '', (resp) => {
+		let total_evaluated_balance = 0; // 총 보유 자산
+		let total_buyable_balance = 0; // 총 구매 가능 자산
+		let total_money = 0;
+
+		// console.log('getBalance resp:', resp);
+		if(resp.payload.length > 0) {
+			resp.payload.filter(function(item) {
+				if (item.crypto_currency === 'N') {
+					return false; // skip
+				}
+				return true;
+			}).map((item) => {
+
+				// 원은 목록에서 제거
+				if (item.symbol==='KRW') {
+					total_buyable_balance = item.confirmed;
+				}
+				// 다른 화폐 제거
+				if(item.symbol ==='USD' || item.symbol ==='ETH'){
+					return;
+				}
+
+				//console.log(item);
+				//console.log("평가수익 : "+item.eval_income);
+
+				//기존 item.confirmed > 0 -> 기준 오류(모둔 상품이 거래가 있을시 0으로 계산 됨)
+				if (item.valuation > 0 || item.symbol=='KRW' ) {
+					item.eval_valuation = item.valuation * item.price;	// 코인의 전체 평가금액
+					if(typeof item.eval_income != typeof undefined){
+						//total_income += item.eval_income;                   // 총 수입
+					}
+					total_money = item.total_money;                        // 현금보유
+
+					total_evaluated_balance += item.eval_valuation; 		// 총 보유 자산
+				}
+
+			})
+
+			//총보유자산
+			let num = total_evaluated_balance*1 + total_buyable_balance*1;
+			//var divElement = document.querySelector('.firsth_title_2');
+			//var pElement = divElement.querySelector('p');
+			//pElement.textContent = real_number_format(num,0);
+			console.log("총금액 : " + num);
+			fn_total();
+			// 원래의 값을 가져와서 쉼표로 구분하여 표시
+			let originalValue = document.getElementById("total_money").textContent;
+			let formattedValue = numberWithCommas(num);
+			document.getElementById("total_money").textContent = formattedValue;
+		}
+	})
+}
+
 const fn_total = function () {
     check_login();
+	$('.wallet--grid').empty();
 
 	API.getMyInoutList(search_type, (resp) => {
-		console.log(resp);
+		console.log(resp.payload.data.length);
 		if(resp.success) {
-            if(resp.payload.length > 0) {
+            if(resp.payload.data.length > 0) {
+				console.log(resp);
                 
 				$('[name="d-grid--empty"]').removeClass('d-grid--empty');
 				$('[name="grid--empty"]').hide();
-                resp.payload.filter(function(item) {
+                resp.payload.data.filter(function(item) {
 					if (item.crypto_currency === 'N') {
 						return false; // skip
 					}
 					return true;
 				}).map((item) => {
-                    
-					const item2 = [{txn_type: 'R', t_pdate: '2023-07-28 00:00:05', regdate: '2023-07-25 12:25:05', amount: '1000'}
-					,{txn_type: 'R', txndate: '1', t_pdate: '2023-07-25 00:00:05', regdate: '2023-07-24 12:25:05', amount: '10000'}
-					,{txn_type: 'R', txndate: '1', t_pdate: '2023-07-24 00:00:05', regdate: '2023-07-23 12:25:05', amount: '3000'}];
+					console.log(item);
+					console.log(search_type);
 
+					const item3 = [item];
 					//for (const d_item of item2) {
 
 					//item2 = [item];
-					for (const d_item of item2) {
+					for (const d_item of item3) {
 						const t_type = d_item.txn_type;
 						const t_time = d_item.regdate;
 						const t_cnt = d_item.amount;
@@ -44,20 +100,23 @@ const fn_total = function () {
 
 						const grid_type2 = grid_type + " 완료";
 
-
-						const grid = $(`<table class="cancel_list" />`);
+						const grid = $(`<table class="myinout_list" />`);
 						grid.append(`
-							<tr class="cancel_list_left">
-								<td rowspan="2" id="cancel_table_check" style="width: 10%; text-align: center; padding-left:5px !important; " class='item_name'><input type="checkbox" class="checkbox" style=" color: ${font_c};" value="${grid_type}"></td>
-								<td id="cancel_table_right1" style="width: 60%; text-align: center;" class='item_grade'>${t_time}</td>
-								<td id="cancel_table_right1" style="width: 30%; text-align: center;" class="rdate">${t_cnt}</td>
+							<tr class="myinout_list_left">
+								<td id="myinout_table_check" style="width: 15%; text-align: center; padding-left: 10px !important; color: ${font_c};" class='item_name'>${grid_type}</td>
+								<td id="myinout_table_right1" style="width: 60%; text-align: left;" class='item_grade'>${t_time}</td>
+								<td id="myinout_table_right1" style="width: 30%; text-align: center; color: ${font_c};" class="rdate">${t_cnt}</td>
 							</tr>
-							<tr class="cancel_list_left">
-								<td id="cancel_table_right2" style="width: 10%; text-align: center;" class='item_grade'>상태</td>
-								<td id="cancel_table_right2" style="width: 60%; text-align: center;" class="rdate">${grid_type2}</td>
+							<tr class="myinout_list_left2">
+								<td id="myinout_table_right2" style="width: 10%; text-align: center; padding-left: 10px !important;" class='item_grade'>상태</td>
+								<td id="myinout_table_right2" style="width: 60%; text-align: left;" class="rdate">${grid_type2}</td>
 							</tr>
 						`);
-						$('.wallet--grid').append(grid);
+
+						if((search_type = 1 && d_item.txn_type == 'R') || (search_type = 2 && d_item.txn_type != 'R') || (search_type == 0)){
+							$('.wallet--grid').append(grid);	
+						}
+						
 					}
                 });
             }
@@ -69,139 +128,6 @@ const fn_total = function () {
     });
 }
 
-//dropdown 버튼 이벤트
-document.addEventListener("DOMContentLoaded", function() {
-    // "dropdown_date" 드롭다운에서 항목을 클릭할 때 이벤트 핸들러 추가
-    var dropdownDateItems = document.querySelectorAll('.dropdown-wrapper[name="dropdown_date"] ul li button');
-    dropdownDateItems.forEach(function(item) {
-        item.addEventListener('click', function() {
-            // 클릭된 항목의 텍스트를 가져와서 "dropdown_date_selected" 버튼의 텍스트로 설정
-            var selectedDropdownDate = item.textContent;
-            document.getElementById('dropdown_date_selected').textContent = selectedDropdownDate;
-        });
-    });
-
-    // "dropdown_type" 드롭다운에서 항목을 클릭할 때 이벤트 핸들러 추가
-    var dropdownTypeItems = document.querySelectorAll('.dropdown-wrapper[name="dropdown_type"] ul li button');
-    dropdownTypeItems.forEach(function(item) {
-        item.addEventListener('click', function() {
-            // 클릭된 항목의 텍스트를 가져와서 "dropdown_type_selected" 버튼의 텍스트로 설정
-            var selectedDropdownType = item.textContent;
-            document.getElementById('dropdown_type_selected').textContent = selectedDropdownType;
-        });
-    });
-});
-
-//option 버튼 이벤트
-function changeStyle(button) {
-	// 모든 버튼을 기본 스타일로 초기화
-	var buttons = document.querySelectorAll(".option_div");
-	const value = button.getAttribute('value');
-	  console.log('클릭한 버튼의 value 값:', value);
-	for (var i = 0; i < buttons.length; i++) {
-	  buttons[i].style.border = "1px solid #999999";
-	  buttons[i].style.color = "#999999";
-	}
-
-	if(value ==''){
-		$('#search_type').val('전체');
-		$('#trade_type').val('');
-		selected_category = '';
-	}else if(value =='buy'){
-		$('#search_type').val('매수');
-		$('#trade_type').val('b');
-		selected_category = 'buy';
-	}else{
-		$('#search_type').val('매도');
-		$('#trade_type').val('s');
-		selected_category = 'sell';
-	}
-  
-	// 클릭된 버튼에 새로운 스타일 적용
-	button.style.border = "1px solid var(--red-up)";
-	button.style.color = "var(--red-up)";
-}
-
-function changeStyle2(button) {
-	// 모든 버튼을 기본 스타일로 초기화
-	var buttons = document.querySelectorAll(".option_div2");
-	const value = button.getAttribute('value');
-	  console.log('클릭한 버튼의 value 값:', value);
-	for (var i = 0; i < buttons.length; i++) {
-	  buttons[i].style.border = "1px solid #999999";
-	  buttons[i].style.color = "#999999";
-	}
-  
-	// 클릭된 버튼에 새로운 스타일 적용
-	button.style.border = "1px solid var(--red-up)";
-	button.style.color = "var(--red-up)";
-
-	//날자 변경
-	var startInput = document.getElementById('start');
-	var endInput = document.getElementById('end');
-	var startInput2 = document.getElementById('start2');
-	var endInput2 = document.getElementById('end2');
-	
-	if(value<1){
-		startInput.value = getOneWeekAgoDate();
-		startInput2.value = getOneWeekAgoDate();
-		$('#search_date').val('일주일');
-	}else if(value>99){
-		$('#search_date').val('임의입력');
-	}else{
-		startInput.value = getNDaysAgo(value);
-		startInput2.value = getNDaysAgo(value);
-		if(value==12){
-			$('#search_date').val('1년');
-		}else{
-			const date_text = value + '개월';
-			$('#search_date').val(date_text);
-		}
-	}
-	 // 오늘 날짜 설정
-	if(value<99){
-		endInput.value = getTodayDate();	
-		endInput2.value = getTodayDate();	
-	}
-	
-}
-
-function changeStyle3(button){
-	const directInputOption = document.querySelector('.transaction-select .option_div2[value="100"]');
-	if (directInputOption) {
-		directInputOption.click();
-		console.log(`Clicked: ${directInputOption.textContent}`);
-	}
-}
-
-// 오늘 날짜를 가져오는 함수
-function getTodayDate() {
-	const today = new Date();
-	const year = today.getFullYear();
-	const month = String(today.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1
-	const day = String(today.getDate()).padStart(2, '0');
-	return `${year}-${month}-${day}`;
-}
-
-// 일주일 전 날짜를 가져오는 함수
-function getOneWeekAgoDate() {
-	const today = new Date();
-	today.setDate(today.getDate() - 7);
-	const year = today.getFullYear();
-	const month = String(today.getMonth() + 1).padStart(2, '0');
-	const day = String(today.getDate()).padStart(2, '0');
-	return `${year}-${month}-${day}`;
-}
-
-function getNDaysAgo(n) {
-  const today = new Date();
-  today.setMonth(today.getMonth() - n);
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, '0');
-  const day = String(today.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
-}
-
 window.addEventListener('load', function() {
 	// 선택 상자 엘리먼트를 가져옵니다.
 	var selectBox = document.getElementById("menuSelect");
@@ -211,39 +137,18 @@ window.addEventListener('load', function() {
 		var selectedValue = selectBox.value;
 		if (selectedValue === "all") {
 			search_type = 0;
-			alert("전체를 선택했습니다.");
+			fn_total();
 		} else if (selectedValue === "deposit") {
 			search_type = 1;
-			alert("입금을 선택했습니다."  + search_type);
+			fn_total();
 		} else if (selectedValue === "withdraw") {
 			search_type = 2;
-			alert("출금을 선택했습니다." + search_type);
+			fn_total();
 		}
 	});
-
-	fn_total();
+	fn_total_money();
 });
 
-const dateChange = function(dateTime){
-    var dateObj = new Date(dateTime);
-    // 년, 월, 일 추출
-    var year = dateObj.getFullYear();
-    var month = ('0' + (dateObj.getMonth() + 1)).slice(-2);
-    var day = ('0' + dateObj.getDate()).slice(-2);
-    
-    // 'yyyy-mm-dd' 형식으로 결과 출력
-    var formattedDate = year + '-' + month + '-' + day;
-    return formattedDate;
-}
-const stateChage = function(text){
-    if(text == 'R'){
-        return "배달 준비중"
-    }else if(text == 'C'){
-        return "취소"
-    }else if(text == 'D'){
-        return "배달 완료"
-    }
-}
 const check_login = function (msg) {
     if (!Model.user_info || !Model.user_info.userid && !Model.user_info.userno) {
         if (msg) alert(msg);
@@ -255,4 +160,9 @@ const check_logout = function (msg) {
         if (msg) alert(msg);
         window.location.href = "/";
     }
+}
+
+// 숫자를 3자리마다 쉼표로 구분하는 함수
+function numberWithCommas(x) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
